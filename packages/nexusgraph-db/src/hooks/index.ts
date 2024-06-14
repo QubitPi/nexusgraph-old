0598372caf14211b760c7a4bf31d646fecfe5a02
Jack <jack20220723@gmail.com>
2024-06-14 18:46:15 +0800

    Combine new graph persistence and rendering into a single hook

--- packages/nexusgraph-db/src/hooks/index.ts
+++ packages/nexusgraph-db/src/hooks/index.ts
@@ -14,4 +14,5 @@
  * limitations under the License.
  */
 
+export { default as useCreateNewGraph } from "./useCreateNewGraph";
 export { default as usePersistNewGraph } from "./usePersistNewGraph";
